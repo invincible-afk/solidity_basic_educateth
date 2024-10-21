cef285598487d38e9bcc208d86deaa18ba3a450c
invincible-afk <indominusfortune@gmail.com>
2024-10-21 19:48:52 -0300

    variables cadena de bloques y array

---
+++ hola_mundo.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24; // en esta parte defino la versión del compilador
+
+// a partir de acá coloco la logica de mi contrato
+contract hola_mundo {
+
+    string public saludo ="Hola desde Educateth 2.0"; // declarando el contenido de mi variable saludo
+
+}
---
+++ mapping.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract Mapping {
+
+    // definir un mapping de direcciones y enteros
+    mapping(address => uint256) balance;
+
+    // funcion para establecer el mapping
+}
